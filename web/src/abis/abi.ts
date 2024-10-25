9c3419d2a44fb4fc8b28bd2213b4a3de8e41d05a
pedro rosalba <pedro.dandrea@gmail.com>
2024-10-25 14:02:57 -0300

    fixing

--- web/src/abis/abi.ts
+++ web/src/abis/abi.ts
@@ -1,68 +1,64 @@
-export const ABI = [
+export const abi = [
   {
     type: "impl",
-    name: "HelloStarknetImpl",
-    interface_name: "workshop_frontend::IHelloStarknet"
+    name: "YourCollectibleImpl",
+    interface_name: "IP::IYourCollectible"
   },
-  {
-    type: "struct",
-    name: "core::integer::u256",
-    members: [
-      { name: "low", type: "core::integer::u128" },
-      { name: "high", type: "core::integer::u128" }
-    ]
-  },
+  // {
+  //   type: "struct",
+  //   name: "core::integer::u256",
+  //   members: [
+  //     { name: "low", type: "core::integer::u128" },
+  //     { name: "high", type: "core::integer::u128" }
+  //   ]
+  // },
   {
     type: "interface",
-    name: "workshop_frontend::IHelloStarknet",
+    name: "IP::IYourCollectible",
     items: [
       {
         type: "function",
-        name: "increase_balance",
-        inputs: [{ name: "amount", type: "core::integer::u256" }],
-        outputs: [],
+        name: "mint_item",
+        inputs: [{ 
+          name: "recipient",
+          type: "core::starknet::contract_address::ContractAddress" 
+        }, 
+        {
+          name: "uri", 
+          type: "core::byte_array::ByteArray"
+        }],
+        outputs: [{
+          name: "token_id",
+          type: "core::integer::u128"
+        }],
         state_mutability: "external"
       },
-      {
-        type: "function",
-        name: "get_balance",
-        inputs: [],
-        outputs: [{ type: "core::integer::u256" }],
-        state_mutability: "view"
-      },
-      {
-        type: "function",
-        name: "reset_balance",
-        inputs: [],
-        outputs: [],
-        state_mutability: "external"
-      }
     ]
   },
-  {
-    type: "event",
-    name: "workshop_frontend::HelloStarknet::BalanceIncreased",
-    kind: "struct",
-    members: [
-      {
-        name: "sender",
-        type: "core::starknet::contract_address::ContractAddress",
-        kind: "key"
-      },
-      { name: "amount", type: "core::integer::u256", kind: "data" },
-      { name: "new_balance", type: "core::integer::u256", kind: "data" }
-    ]
-  },
-  {
-    type: "event",
-    name: "workshop_frontend::HelloStarknet::Event",
-    kind: "enum",
-    variants: [
-      {
-        name: "BalanceIncreased",
-        type: "workshop_frontend::HelloStarknet::BalanceIncreased",
-        kind: "nested"
-      }
-    ]
-  }
+  // { 
+  //   type: "event",
+  //   name: "workshop_frontend::HelloStarknet::BalanceIncreased",
+  //   kind: "struct",
+  //   members: [
+  //     {
+  //       name: "sender",
+  //       type: "core::starknet::contract_address::ContractAddress",
+  //       kind: "key"
+  //     },
+  //     { name: "amount", type: "core::integer::u256", kind: "data" },
+  //     { name: "new_balance", type: "core::integer::u256", kind: "data" }
+  //   ]
+  // },
+  // {
+  //   type: "event",
+  //   name: "workshop_frontend::HelloStarknet::Event",
+  //   kind: "enum",
+  //   variants: [
+  //     {
+  //       name: "BalanceIncreased",
+  //       type: "workshop_frontend::HelloStarknet::BalanceIncreased",
+  //       kind: "nested"
+  //     }
+  //   ]
+  // }
 ] as const;
