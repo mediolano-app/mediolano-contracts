43903a7bb6477c3b2fdcafd56defc12df9fbc23c
Salvador Camino <salvadorcamino@gmail.com>
2024-10-27 13:25:48 -0300

    ipregister

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+"use client"
 
 import Layout from '../components/layout'
 import { useState } from 'react'
@@ -13,23 +12,22 @@ import { type Abi } from "starknet"
 import { abi } from '@/abis/abi'
 
 
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import * as z from 'zod'
-import { format } from 'date-fns'
-import { Calendar as CalendarIcon, Upload } from 'lucide-react'
-
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { Textarea } from '@/components/ui/textarea'
-import { Calendar } from '@/components/ui/calendar'
+  toast
+} from "sonner"
+import {
+  useForm
+} from "react-hook-form"
+import {
+  zodResolver
+} from "@hookform/resolvers/zod"
+import * as z from "zod"
+import {
+  cn
+} from "@/lib/utils"
+import {
+  Button
+} from "@/components/ui/button"
 import {
   Form,
   FormControl,
@@ -38,347 +36,134 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '@/components/ui/form'
+} from "@/components/ui/form"
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
-import { cn } from '@/lib/utils'
+  Input
+} from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const formSchema = z.object({
-  title: z.string().min(1, 'Title is required'),
-  type: z.string().optional(),
-  description: z.string().min(1, 'Description is required'),
-  author: z.string().min(1, 'Author is required'),
-  date: z.date({
-    required_error: 'Date is required',
-  }),
-  tags: z.string().optional(),
-  file: z.any().optional(),
-  mediaUrl: z.string().url().optional().or(z.literal('')),
-  version: z.string().optional(),
-  standard: z.string({
-    required_error: 'Standard is required',
-  }),
-  chain: z.string({
-    required_error: 'Chain is required',
-  }),
-  dapp: z.string({
-    required_error: 'Dapp is required',
-  }),
-})
+  title: z.string(),
+  type: z.string()
+});
+
+export default function IPRegister() {
+
+  const form = useForm < z.infer < typeof formSchema >> ({
+    resolver: zodResolver(formSchema),
+
+  })
+
+  function onSubmit(values: z.infer < typeof formSchema > ) {
+    try {
+      console.log(values);
+      toast(
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+        </pre>
+      );
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
 
 
 
-export type IPType = "" | "patent" | "trademark" | "copyright" | "trade_secret";
-
-export interface IP{
-  title: string,
-  description: string,
-  authors: string[] | string,
-  ipType: IPType,
-  uploadFile?: File,
-}
-
-
-export default function Register() {
-
+  const templates = [
+    { name: 'Art', icon: Palette, href: '/registerArt', description: 'Tokenize your Artwork' },
+    { name: 'Documents', icon: File, href: '/registerDocument', description: 'Safeguard Documents On-Chain' },  
+    { name: 'Films', icon: Film, href: '/registerFilm', description: 'Protect your cinematic creations' }, 
+    { name: 'Music', icon: Music, href: '/registerMusic', description: 'Copyright Compositions' },
+    { name: 'Patents', icon: ScrollText, href: '/registerPatent', description: 'Secure Inventions and Innovations' },
+    { name: 'Publications', icon: Book, href: '/registerPublication', description: 'Protect your Written Works' },
+    { name: 'RWA', icon: Globe2, href: '/registerRWA', description: 'Tokenize Real World Assets' },
+    { name: 'Software', icon: FileCode, href: '/registerSoftware', description: 'Safeguard your Code' },
+    { name: 'Custom', icon: Coins, href: '/registerIP', description: 'Edit Your Template' },
+  ];
 
 
-  
 
   return (
+
+    <>
+    
     <Layout>
       <div className="container mx-auto px-4 py-8">
-
-
         <h1 className="text-4xl font-bold text-center mb-8">Intellectual Property Registration</h1>
 
 
     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-    <div className="bg-card bg-primary text-card-foreground rounded-lg shadow-lg">
-
+    <div className="rounded-lg">
     <Card>
     <CardHeader>
       <CardTitle>Create new IP</CardTitle>
       <CardDescription>Register your intellectual property on Starknet blockchain.</CardDescription>
     </CardHeader>
     <CardContent>
-    
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Title</FormLabel>
+
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+        
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ttitle</FormLabel>
+              <FormControl>
+                <Input 
+                placeholder="Title"
+                
+                type="text"
+                {...field} />
+              </FormControl>
+              <FormDescription>The title of your intellectual property</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Type</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
                 <FormControl>
-                  <Input placeholder="Enter title" {...field} />
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select the type of your IP" />
+                  </SelectTrigger>
                 </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+                <FormDescription>The type of your Intellectual Property</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
 
-          <FormField
-            control={form.control}
-            name="type"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Type</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select type" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="Generic">Generic</SelectItem>
-                    <SelectItem value="Patent">Patent</SelectItem>
-                    <SelectItem value="Trademark">Trademark</SelectItem>
-                    <SelectItem value="Copyright">Copyright</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Description</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Enter description"
-                    className="resize-none"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="author"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Author</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter author name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="date"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Date</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant={'outline'}
-                        className={cn(
-                          'w-[240px] pl-3 text-left font-normal',
-                          !field.value && 'text-muted-foreground'
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, 'PPP')
-                        ) : (
-                          <span>Pick a date</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      disabled={(date) =>
-                        date > new Date() || date < new Date('1900-01-01')
-                      }
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="tags"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tags</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter tags (comma-separated)" {...field} />
-                </FormControl>
-                <FormDescription>
-                  Separate multiple tags with commas
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="file"
-            render={({ field: { value, onChange, ...field } }) => (
-              <FormItem>
-                <FormLabel>File Upload</FormLabel>
-                <FormControl>
-                  <Input
-                    type="file"
-                    {...field}
-                    onChange={(event) => {
-                      const file = event.target.files?.[0]
-                      onChange(file)
-                    }}
-                  />
-                </FormControl>
-                <FormDescription>Upload a file (optional)</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="mediaUrl"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Media URL</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter media URL" {...field} />
-                </FormControl>
-                <FormDescription>Enter a URL for media (optional)</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="version"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Version</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter version" {...field} />
-                </FormControl>
-                <FormDescription>Enter a version number (optional)</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="standard"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Standard</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select standard" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="ERC-721">ERC-721</SelectItem>
-                    <SelectItem value="ERC-1155">ERC-1155</SelectItem>
-                    <SelectItem value="ERC-20">ERC-20</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="chain"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Chain</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select chain" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="Starknet">Starknet</SelectItem>
-                    <SelectItem value="Ethereum">Ethereum</SelectItem>
-                    <SelectItem value="Polygon">Polygon</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="dapp"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Dapp</FormLabel>
-                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select dapp" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="Mediolano">Mediolano</SelectItem>
-                    <SelectItem value="Other1">Other1</SelectItem>
-                    <SelectItem value="Other2">Other2</SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <Button type="submit">Register IP</Button>
-        </form>
-      </Form>
-    
-  
-  </CardContent>
+    </CardContent>
     <CardFooter className="flex justify-between">
     </CardFooter>
   </Card>
   </div>
 
-
-
-  
-
-
-
-    
-    
-    
   <div className="text-card-foreground rounded-lg">
   <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <h2 className="text-1xl mb-8">
@@ -519,10 +304,13 @@ export default function Register() {
       </Card>
     </div>
 
-
-
   </div>
-</div>
-    </Layout>
+
+          </div>
+
+  </Layout>
+    
+    </>
+
   )
 }
