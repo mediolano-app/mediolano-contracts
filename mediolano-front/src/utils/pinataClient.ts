7b6d55a13615b495cdda76e131713fc9d1d874c7
Pedro Rosalba <pedro.dandrea.rpd@gmail.com>
2024-11-20 18:14:27 -0300

    separating

--- mediolano-front/src/utils/pinataClient.ts
+++
@@ -1,9 +0,0 @@
-import { PinataSDK } from "pinata-web3";
-
-export const pinataClient = new PinataSDK({
-	pinataJwt: `${process.env.PINATA_JWT}`,
-	pinataGateway: 
-	// 'https://violet-rainy-shrimp-423.mypinata.cloud'
-	`${process.env.PINATA_GATEWAY}`	
-	,	
-});
